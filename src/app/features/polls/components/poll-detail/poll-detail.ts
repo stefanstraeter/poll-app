88902013c74ac99d6105e2640ef93a8c0e0495b6
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-09 15:45:53 +0200

    feat: add PollDetail component with template and styles

---
+++ src/app/features/polls/components/poll-detail/poll-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-poll-detail',
+  styleUrl: './poll-detail.scss',
+  templateUrl: './poll-detail.html',
+})
+export class PollDetail {}
