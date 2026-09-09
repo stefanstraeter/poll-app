414b12d5214c54742f1e375bfa9eafee832e5efe
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-09 15:45:29 +0200

    feat: create Home component with template and styles

---
+++ src/app/features/home/components/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Button } from '@shared/components/button/button';
+
+@Component({
+  imports: [Button],
+  selector: 'app-home',
+  styleUrl: './home.scss',
+  templateUrl: './home.html',
+})
+export class Home {}
