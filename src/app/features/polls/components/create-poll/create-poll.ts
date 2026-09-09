a6ea295866c62c56e973701955efb794d548fdc9
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-09 15:45:40 +0200

    feat: add CreatePoll component with template and styles

---
+++ src/app/features/polls/components/create-poll/create-poll.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-create-poll',
+  styleUrl: './create-poll.scss',
+  templateUrl: './create-poll.html',
+})
+export class CreatePoll {
+  navigateToCreateSurvey() {
+    // Implement navigation logic here, e.g., using Angular Router
+  }
+}
