0714d58fc6ac5e7ddb874502e5ea2b174b0ca45b
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-09 15:45:10 +0200

    feat: define application routes for Home, PollDetail, and CreatePoll components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from '@features/home/components/home/home';
+import { CreatePoll } from '@features/polls/components/create-poll/create-poll';
+import { PollDetail } from '@features/polls/components/poll-detail/poll-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'poll/:id', component: PollDetail },
+  { path: 'create', component: CreatePoll },
+];
