c9caed0175bcbb57962b18fd1deac3269de6b98b
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-14 16:31:40 +0200

    feat: update home component to include poll cards and adjust title line height

--- src/app/features/home/components/home/home.ts
+++ src/app/features/home/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AnswerOption } from '@shared/components/answer-option/answer-option';
 import { Button } from '@shared/components/button/button';
+import { PollCard } from '@shared/components/poll-card/poll-card';
 
 @Component({
-  imports: [Button, AnswerOption],
+  imports: [Button, PollCard],
   selector: 'app-home',
   styleUrl: './home.scss',
   templateUrl: './home.html',
