7c1305c9f83562dfdceca00c7e411138924a25e4
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-15 16:54:22 +0200

    feat: add  dropdonw menu for testing

--- src/app/features/home/components/home/home.ts
+++ src/app/features/home/components/home/home.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Button } from '@shared/components/button/button';
+import { Dropdown, DropdownOption } from '@shared/components/dropdown/dropdown';
 
 @Component({
-  imports: [Button],
+  imports: [Button, Dropdown],
   selector: 'app-home',
   styleUrl: './home.scss',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  categoryOptions: DropdownOption[] = [
+    { value: 'all', label: 'All Surveys' },
+    { value: 'team', label: 'Team Activities' },
+    { value: 'health', label: 'Health & Wellness' },
+    { value: 'gaming', label: 'Gaming & Entertainment' },
+    { value: 'education', label: 'Education & Learning' },
+    { value: 'lifestyle', label: 'Lifestyle & Preferences' },
+    { value: 'tech', label: 'Technology & Innovation' },
+  ];
+
+  selectedCategory = signal('');
+}
