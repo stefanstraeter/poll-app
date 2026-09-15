d939994f5680da587f5bbb24e1f440c1e57d85ec
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-15 10:24:26 +0200

    feat: add progress bar component

---
+++ src/app/shared/components/progress-bar/progress-bar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-progress-bar',
+  styleUrl: './progress-bar.scss',
+  templateUrl: './progress-bar.html',
+})
+export class ProgressBar {}
