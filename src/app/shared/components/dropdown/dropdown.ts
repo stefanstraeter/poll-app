09c0577727f6619d8a310bb2a206c4894af08c11
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-15 16:53:33 +0200

    feat: add dropdown component

---
+++ src/app/shared/components/dropdown/dropdown.ts
@@ -0,0 +1,34 @@
+import { Component, input, output, signal } from '@angular/core';
+
+export interface DropdownOption {
+  value: string;
+  label: string;
+}
+
+@Component({
+  selector: 'app-dropdown',
+  imports: [],
+  templateUrl: './dropdown.html',
+  styleUrl: './dropdown.scss',
+})
+export class Dropdown {
+  label = input.required<string>();
+  options = input.required<DropdownOption[]>();
+  selected = input<string>('');
+  selectedChange = output<string>();
+
+  isOpen = signal(false);
+
+  toggleOpen(): void {
+    this.isOpen.set(!this.isOpen());
+  }
+
+  closeDropdown(): void {
+    this.isOpen.set(false);
+  }
+
+  selectOption(value: string): void {
+    this.selectedChange.emit(value);
+    this.isOpen.set(false);
+  }
+}
