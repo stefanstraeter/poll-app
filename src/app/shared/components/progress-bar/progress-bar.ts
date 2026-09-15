540b8fa090d1f82747c3d80f17b75ce4f10b0500
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-15 10:37:16 +0200

    feat: add progress bar html, scss styling and functionality

--- src/app/shared/components/progress-bar/progress-bar.ts
+++ src/app/shared/components/progress-bar/progress-bar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   imports: [],
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './progress-bar.scss',
   templateUrl: './progress-bar.html',
 })
-export class ProgressBar {}
+export class ProgressBar {
+  percentage = input.required<number>();
+}
