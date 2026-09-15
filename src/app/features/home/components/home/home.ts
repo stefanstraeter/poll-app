af60e18360b08fce5cf79b393f5d75cec2684a2b
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-15 10:17:29 +0200

    refactor: remove unused poll card components from home template and module

--- src/app/features/home/components/home/home.ts
+++ src/app/features/home/components/home/home.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { AnswerOption } from '@shared/components/answer-option/answer-option';
 import { Button } from '@shared/components/button/button';
-import { PollCard } from '@shared/components/poll-card/poll-card';
 
 @Component({
-  imports: [Button, PollCard],
+  imports: [Button],
   selector: 'app-home',
   styleUrl: './home.scss',
   templateUrl: './home.html',
