3b61b7392a10cef6b9600088468e040f1f6e18dd
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-17 10:32:30 +0200

    chore: remove dropdown from home and dummy data

--- src/app/features/home/components/home/home.ts
+++ src/app/features/home/components/home/home.ts
@@ -1,23 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Button } from '@shared/components/button/button';
-import { Dropdown, DropdownOption } from '@shared/components/dropdown/dropdown';
 
 @Component({
-  imports: [Button, Dropdown],
+  imports: [Button],
   selector: 'app-home',
   styleUrl: './home.scss',
   templateUrl: './home.html',
 })
-export class Home {
-  categoryOptions: DropdownOption[] = [
-    { value: 'all', label: 'All Surveys' },
-    { value: 'team', label: 'Team Activities' },
-    { value: 'health', label: 'Health & Wellness' },
-    { value: 'gaming', label: 'Gaming & Entertainment' },
-    { value: 'education', label: 'Education & Learning' },
-    { value: 'lifestyle', label: 'Lifestyle & Preferences' },
-    { value: 'tech', label: 'Technology & Innovation' },
-  ];
-
-  selectedCategory = signal('');
-}
+export class Home {}
