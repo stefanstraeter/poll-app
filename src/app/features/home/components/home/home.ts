f7ea0ab8da2f8307cf6292f5766967ad76fc38a7
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-17 16:28:40 +0200

    feat: add import for SurveysOverview

--- src/app/features/home/components/home/home.ts
+++ src/app/features/home/components/home/home.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { SurveysOverview } from '@features/polls/components/surveys-overview/surveys-overview';
 import { Button } from '@shared/components/button/button';
 
 @Component({
-  imports: [Button],
+  imports: [Button, SurveysOverview],
   selector: 'app-home',
   styleUrl: './home.scss',
   templateUrl: './home.html',
