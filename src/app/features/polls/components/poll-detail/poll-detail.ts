6850ec60d1412ff0dbeb3bb4700394720a6e884a
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-24 13:29:33 +0200

    refactor: rename PollDetail to SurveyDetail.

--- src/app/features/polls/components/poll-detail/poll-detail.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-poll-detail',
-  styleUrl: './poll-detail.scss',
-  templateUrl: './poll-detail.html',
-})
-export class PollDetail {}
---
+++ src/app/features/polls/components/survey-detail/survey-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-survey-detail',
+  styleUrl: './survey-detail.scss',
+  templateUrl: './survey-detail.html',
+})
+export class SurveyDetail {}
