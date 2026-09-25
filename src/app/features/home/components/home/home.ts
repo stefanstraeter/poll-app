dff2a9818db0a972b7df5f70a098edc467b26da0
Stefan Sträter <stefan.straeter@gmail.com>
2026-09-25 11:15:29 +0200

    refactor: add RouterLink to home component imports for navigation

--- src/app/features/home/components/home/home.ts
+++ src/app/features/home/components/home/home.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { SurveysOverview } from '@features/polls/components/surveys-overview/surveys-overview';
 import { Button } from '@shared/components/button/button';
 
 @Component({
-  imports: [Button, SurveysOverview],
+  imports: [Button, SurveysOverview, RouterLink],
   selector: 'app-home',
   styleUrl: './home.scss',
   templateUrl: './home.html',
